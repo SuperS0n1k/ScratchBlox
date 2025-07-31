567cde51d35e6a09ad469895a3c218320bd4e8df
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2025-07-31 10:58:32 +0200

    Create part1.js

---
+++ part1.js
@@ -0,0 +1,2 @@
+fetch('https://cdn.socket.io/4.7.2/socket.io.min.js').then(r=>r.text()).then(t=>eval(t))
+fetch('https://raw.githubusercontent.com/SuperS0n1k/ScratchBlox/main/part2.js').then(r=>r.text()).then(t=>eval(t))
